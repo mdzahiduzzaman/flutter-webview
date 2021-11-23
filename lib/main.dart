47c086cda7dd071cec2ebef1d9a99d34d17cebb7
Md. Zahiduzzaman <md.zahiduzzaman@outlook.com>
2021-11-23 16:26:27 +0600

    Null safety migration

--- lib/main.dart
+++ lib/main.dart
@@ -29,22 +29,25 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double progress = 0;
+  String? currentUrl = 'https://www.google.com/';
+
   final globalKey = GlobalKey<ScaffoldState>();
   final Completer<WebViewController> controller =
       Completer<WebViewController>();
 
-  WebViewController controllerGlobal;
+  WebViewController? controllerGlobal;
   Future<bool> goBack() async {
-    if (await controllerGlobal.canGoBack()) {
-      controllerGlobal.goBack();
+    if (await controllerGlobal!.canGoBack()) {
+      controllerGlobal!.goBack();
       return Future.value(false);
     } else {
       return Future.value(true);
@@ -57,13 +60,60 @@ class _MyHomePageState extends State<MyHomePage> {
       onWillPop: goBack,
       child: SafeArea(
         child: Scaffold(
-          body: WebView(
-            initialUrl: 'https://tacpulse.animo-ai.co/',
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController) {
-              controller.future.then((value) => controllerGlobal = value);
-              controller.complete(webViewController);
-            },
+          appBar: AppBar(
+            title: ListTile(
+              title: Text(
+                'Flutter WebView',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              subtitle: Text(currentUrl!,
+                  style: TextStyle(overflow: TextOverflow.ellipsis)),
+              contentPadding: const EdgeInsets.all(0),
+            ),
+            leading: IconButton(
+              padding: const EdgeInsets.only(left: 10),
+              icon: Icon(Icons.clear),
+              onPressed: () {
+                controllerGlobal!.clearCache();
+                CookieManager().clearCookies();
+              },
+            ),
+            actions: [
+              IconButton(
+                padding: const EdgeInsets.only(right: 10),
+                onPressed: () {
+                  controllerGlobal!.reload();
+                },
+                icon: Icon(Icons.refresh),
+              ),
+            ],
+            leadingWidth: 37.5,
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+          ),
+          body: Column(
+            children: [
+              LinearProgressIndicator(
+                value: progress,
+              ),
+              Expanded(
+                child: WebView(
+                  initialUrl: 'https://www.google.com/',
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (WebViewController webViewController) {
+                    controller.future.then((value) => controllerGlobal = value);
+                    controller.complete(webViewController);
+                  },
+                  onPageFinished: (url) {
+                    setState(() => currentUrl = url);
+                  },
+                  onProgress: (progress) =>
+                      setState(() => this.progress = progress / 100),
+                ),
+              ),
+            ],
           ),
         ),
       ),
